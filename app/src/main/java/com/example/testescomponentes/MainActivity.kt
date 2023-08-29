fcc288538d4cc7ec635f5e742569b8dcba903d8b
micilini <sdanz@hotmail.com>
2023-08-29 15:25:36 -0300

    Initial Commit

---
+++ app/src/main/java/com/example/testescomponentes/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.testescomponentes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.CompoundButton
+import android.widget.SeekBar
+import android.widget.Switch
+import androidx.appcompat.widget.SwitchCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+}
